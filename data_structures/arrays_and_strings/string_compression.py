98edc2889ae40e8735cd57936012d319f8d66470
Ly Nguyen <lynguyen237@Lys-MacBook-Pro.local>
2021-03-30 18:23:39 +0700

    Correctly return a smaller string in terms of length

--- data_structures/arrays_and_strings/string_compression.py
+++ data_structures/arrays_and_strings/string_compression.py
@@ -5,12 +5,12 @@
 # You can assume the string has only uppercase and lowercase letters (a - z).
 # Hints: #92, # 110
 
+#==== Solution (O(n))
 def string_compression(string):
     """ Perfoprm basic string compression using the counts of repeated characters"""
     if string == "": 
         return ""
     
-    not_compressed = True
     new_str = ""
 
     i = 0
@@ -20,23 +20,19 @@ def string_compression(string):
     while i < len(string) - 1:
         if string[i+1] == string[i]:
             count +=1
-            i += 1
-            # if the a character is repeated, then the string can definitely be compressed into a smaller string,
-            # so we set the not_compressed value to False
-            not_compressed = False
+            i += 1   
         else:
             new_str += str(count) + string[i+1]
             count = 1
             i += 1
     new_str += str(count)
-
-    if not_compressed:
-        return string
     
-    return new_str
+    return min(string, new_str, key = len) #return the smaller string
 
 
 print(string_compression("aabcccccaaa")) #a2b1c5a3
 print(string_compression("")) #""
 print(string_compression("abca")) #abca
 print(string_compression("aBDDdca")) #a1B1D2d1c1a1
+
+# https://github.com/careercup/CtCI-6th-Edition-Python/blob/master/chapter_01/p06_string_compression.py
